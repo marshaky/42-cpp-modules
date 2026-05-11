47cf3f59ef58167906f470a4bcdc94b754acf054
MilenaArshakyan <arshakyanmilena2005@gmail.com>
2026-05-11 20:56:30 +0400

    add cpp01 ex00

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marshaky <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/08 23:47:16 by marshaky          #+#    #+#             */
+/*   Updated: 2026/05/11 20:51:49 by marshaky         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie(std::string name)
+{
+	this->_name = name;
+	std::cout << this->_name << " created." << std::endl;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << this->_name << " destroyed" << std::endl;
+}
+
+void	Zombie::announce(void)
+{
+	std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
